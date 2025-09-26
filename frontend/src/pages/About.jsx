b5b73042f7e5009af095972711d0c28073a8ab00
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2025-09-26 10:39:55 -0700

    created about & contect pages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from 'react';
+import {assets} from "../assets/assets.js"
 
 const About = () => {
   return (
     <div>
-      <h2>About</h2>
+      
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        ABOUT <span className='text-gray-700 font-medium'>US</span>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full max-w-[300px]' src={assets.about_image} />
+
+        <div className='flex flex-col gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero suscipit impedit, quo similique earum temporibus officiis fugit assumenda veritatis et ab omnis, nostrum accusamus accusantium blanditiis sed id natus ad.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut unde odio, natus error commodi soluta reiciendis voluptates nisi animi, in veritatis facere possimus quae eum, incidunt magnam placeat dolor architecto temporibus. Numquam corrupti, obcaecati ratione omnis consequatur delectus fugit eum!</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsa pariatur obcaecati perferendis repellat. At, magnam sunt rerum itaque voluptas perferendis vero, molestias quo voluptates dolore minus reiciendis. Enim ut inventore rem architecto ullam iure?</p>
+        </div>
+      </div>
+
+      <div className='text-xl my-4 text-gray-700'>
+        <p>WHY <span className='text-gray-900 font-semibold'>CHOOSE US</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border border-gray-400 px-6 md:px-10 py-8 sm:py-10 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiency:</b>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione accusantium nesciunt </p>
+        </div>
+
+        <div className='border border-gray-400 px-6 md:px-10 py-8 sm:py-10 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Consistence:</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, repellat?</p>
+        </div>
+
+        <div className='border border-gray-400 px-6 md:px-10 py-8 sm:py-10 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personlization:</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consectetur architecto odio?</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { assets } from '../assets/assets';
 
 const Contact = () => {
   return (
     <div>
-      <h2>Contact</h2>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>CONTACT <span className='text-gray-900 font-medium'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col gap-6 md:flex-row justify-center text-sm mb-28 text-gray-600'>
+        <img className='w-full max-w-[300px]' src={assets.contact_image} alt='contact' />
+        
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className='text-gray-500 text-sm'>1234 Street Name, City, State, 12345 <br />Phone: (123) 456-7890</p>
+          <p className='text-gray-500 text-sm'>Website:Email: abcxyz@gmail.com <br/>Website: www.example.com</p>
+          <p className='font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO </p>
+          <p className='text-gray-500'>Learn More About team and job opening</p>
+          <button className='border border-gray-600 px-6 md:px-8 py-3 cursor-pointer hover:bg-black hover:text-white transition-all duration-300'>Explore Jobs</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
